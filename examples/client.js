154469f831ef56eaa9fbab846bf25ef0847168bb
Eric Martindale <eric@ericmartindale.com>
2015-01-20 11:01:33 -0500

    Update to latest bitcore-channel.

--- examples/client.js
+++ examples/client.js
@@ -8,9 +8,7 @@ var async = require('async');
 
 var TEST_PORT = 15237;
 
-var fundingKey = new PrivateKey('cb5dc68fbcaf37f29139b50fa4664b395c03e49deb966e5d49a629af005d0654');
-var refundKey = new PrivateKey('b65080da83f59a9bfa03841bc82fd0c0d1e036176b2f2c157eaa9547010a042e');
-var commitmentKey = new PrivateKey('f1a140dc9d795c0aa537329379f645eb961fe42f27c660e10676c07ddf18777f');
+var refundAddress = 'mzCXqcsLBerwyoRZzBFQELHaJ1ZtBSxxe6';
 
 var proposal = null;
 async.series([
@@ -29,14 +27,16 @@ async.series([
     var providerKey = new PublicKey( proposal.provider.key );
     
     var consumer = new Consumer({
-      fundingKey: fundingKey,
-      refundKey: refundKey,
-      refundAddress: refundKey.toAddress(),
-      commitmentKey: commitmentKey,
-      providerPublicKey: providerKey,
-      providerAddress: providerKey.toAddress()
+      providerPublicKey: providerKey.toString(),
+      providerAddress: providerKey.toAddress(),
+      refundAddress: refundAddress
     });
     
+    console.info('Send bitcoins to ' + consumer.fundingAddress.toString() ' to fund the channel');
+    // consumer.processFunding([{...}, {...}, {...}]);
+    
+    var messageToProvider = consumer.setupRefund();
+
     done();
   }
 ], function(err, results) {
